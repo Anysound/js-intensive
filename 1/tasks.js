c8f5ca5634fe75a3d62e3e57bd6f2eab51410b90
anysound <anysoundspb@gmail.com>
2020-12-15 21:25:12 +0300

    changed some variables

--- 1/tasks.js
+++ 1/tasks.js
@@ -13,10 +13,10 @@ getNumber();
 
 /*task 2*/
 const getResult = () => {
-  let number1 = +prompt('enter first number');
-  let number2 = +prompt('enter second number');
+  const number1 = +prompt('enter first number');
+  const number2 = +prompt('enter second number');
   if (number1 && number2) {
-    let value = prompt('some value');
+    const value = prompt('some value');
     console.log(value, number1 + number2, number1 / number2);
   } else {
     throw new Error('some value is not a number');
